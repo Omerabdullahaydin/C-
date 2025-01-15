955ee763e7c20d74b8f7b341fffbd52a3aa9298c
Ömer <omer.aydinnn565@gmail.com>
2025-01-15 18:54:01 +0300

    Yapılacaklar Listesi projesi

---
+++ Yapılacaklar Listesi/Yapılacaklar Listesi/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yapılacaklar_Listesi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = Text.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = text,
+                    Margin = new Thickness(10),
+
+                };
+                CheckBox check = new CheckBox()
+                {
+                    Margin = new Thickness(10,10,2,10),
+                };
+                Panel.Children.Add(check);
+                Panel.Children.Add(textBlock);
+
+                Text.Clear();
+            }
+        }
+    }
+}
